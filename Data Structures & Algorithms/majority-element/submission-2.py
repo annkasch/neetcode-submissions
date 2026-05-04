2e9f2f0c8f74b3efe5543335c0b944866fbcf747
Ann-Kathrin Schuetz <aschuetz@lbl.gov>
2026-05-04 12:51:06 -0700

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count_map = defaultdict(int)
+        n = len(nums)
+        max_count = 0
+        max_num = 0
+        for num in nums:
+            count_map[num] += 1
+            if max_count < count_map[num]:
+                max_num = num
+                max_count = count_map[num]
+            if max_count >= n/2:
+                return max_num
+        
+        return max_num
+        
