0ae9c983d0025772bd0430b430f4366549069363
Ann-Kathrin Schuetz <aschuetz@lbl.gov>
2026-05-13 21:40:02 -0700

    Add: merge-sorted-array - submission-5

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m-1
+        j = n-1
+        while i >= 0 or j >= 0:
+            if j < 0 or (i >= 0 and nums1[i] > nums2[j]):
+                nums1[i+j+1] = nums1[i]
+                i -= 1
+            else:
+                nums1[i+j+1] = nums2[j]
+                j -= 1
+                
+            
+            
+
+                
+        
