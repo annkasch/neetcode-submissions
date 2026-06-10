ba0b0afe35647ef2284838a6c6481233131ab430
Ann-Kathrin Schuetz <aschuetz@lbl.gov>
2026-06-09 19:03:55 -0700

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l = 0
+        r = x
+        if x == 0:
+            return 0
+        elif x == 1:
+            return 1
+        while l <= r:
+            m = (l + r) // 2
+            sqrt = m * m
+            if sqrt > x:
+                r = m - 1
+            else:
+                l = m + 1
+        return r
